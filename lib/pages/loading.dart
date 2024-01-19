7c04cc5f489fd76243cb47b0b0e31ed4a7fcd36b
Ariel Arevalo <arielarevalo02@gmail.com>
2024-01-19 17:27:20 +0800

    Added background video in loading screen

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,18 +1,14 @@
 import 'dart:async';
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:com.relief.motivationalapp/pages/onboard.dart';
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
 import 'package:com.relief.motivationalapp/services/user_preferences.dart';
 import 'package:com.relief.motivationalapp/pages/home.dart';
 import 'package:gif_view/gif_view.dart';
-import 'package:page_transition/page_transition.dart';
 import 'package:video_player/video_player.dart';
-
-import 'dart:developer' as dev;
+import 'package:page_transition/page_transition.dart';
 
 class Loading extends StatefulWidget {
-  const Loading({super.key});
+  const Loading({Key? key}) : super(key: key);
 
   @override
   State<Loading> createState() => _LoadingState();
@@ -20,61 +16,72 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> with TickerProviderStateMixin {
   late VideoPlayerController _videoController;
-  late AudioPlayer _audioPlayer;
 
   @override
   void initState() {
     super.initState();
-    _audioPlayer = AudioPlayer();
+    _initVideoPlayer();
   }
 
   @override
   void dispose() {
-    _audioPlayer.dispose();
-    _videoController.dispose();
     super.dispose();
+    _videoController.dispose();
   }
 
-  Future<void> _playAudio() async {
-    await _audioPlayer.play(AssetSource('audio/fairy-glitter.wav'));
+  void _initVideoPlayer() {
+    _videoController = VideoPlayerController.asset("assets/videos/Background.mp4")
+      ..initialize().then((_) {
+        _videoController.setLooping(false);
+        _videoController.setVolume(1.0);
+        _videoController.play();
+        setState(() {});
+        _navigateAfterDelay();
+      });
   }
 
-  Future<void> _playVideo() async {
-    _videoController = VideoPlayerController.asset('assets/videos/Background.mp4');
-    await _videoController.play();
-    dev.log('video play');
+  void _navigateAfterDelay() {
+    Future.delayed(const Duration(seconds: 8), () {
+      if (UserPrefs.isOnboarded) {
+        Navigator.pushReplacement(
+          context,
+          PageTransition(type: PageTransitionType.fade, child: const Home()),
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          PageTransition(type: PageTransitionType.fade, child: const Onboard()),
+        );
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    _playVideo();
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      Future.delayed(const Duration(seconds: 2), (){
-        _playAudio();
-      });
-    });
-    return AnimatedSplashScreen.withScreenFunction(
-      splash: GifView.asset(
-        'assets/videos/relief-loading.gif',
-        height: 400,
-        width: 400,
-        frameRate: 60,
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          SizedBox.expand(
+            child: FittedBox(
+              fit: BoxFit.cover,
+              child: SizedBox(
+                  width: _videoController.value.size.width,
+                  height: _videoController.value.size.height,
+                  child: VideoPlayer(_videoController),
+              ),
+            ),
+          ),
+          Center(
+            child: GifView.asset(
+              'assets/videos/relief-loading-transparent.gif',
+              height: 400,
+              width: 400,
+              frameRate: 60,
+            ),
+          ),
+        ],
       ),
-
-      backgroundColor: Colors.white,
-      splashIconSize: 250,
-      duration: 7000,
-      animationDuration: const Duration(seconds: 1),
-      screenFunction: () async{
-        if(UserPrefs.isOnboarded){
-          return const Home();
-        }
-        else {
-          return const Onboard();
-        }
-      },
-      splashTransition: SplashTransition.fadeTransition,
-      pageTransitionType: PageTransitionType.leftToRightWithFade, //doesnt work, just blinks to next page
     );
   }
 }
